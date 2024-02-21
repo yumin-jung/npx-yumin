bbce1cc3001976c5196e08c1e849dd872bb27ba5
yumin-jung <jungym887@naver.com>
2024-02-21 19:09:27 +0900

    init: Init npx-yumin v0.0.1

---
+++ readme.md
@@ -0,0 +1,26 @@
+# yumin CLI
+
+A CLI for yumin.
+
+## Customizing your CLI
+
+Check out the documentation at https://github.com/infinitered/gluegun/tree/master/docs.
+
+## Publishing to NPM
+
+To package your CLI up for NPM, do this:
+
+```shell
+$ npm login
+$ npm whoami
+$ npm test
+
+$ npm run build
+
+$ npm publish
+```
+
+# License
+
+MIT - see LICENSE
+
---
+++ src/commands/yumin.ts
@@ -0,0 +1,37 @@
+import { GluegunCommand } from 'gluegun'
+
+const command: GluegunCommand = {
+  name: 'yumin',
+  run: async (toolbox) => {
+    const { print } = toolbox
+
+    print.newline()
+    print.newline()
+    print.newline()
+
+    print.info(`   Hello, I'm \u001b[44m Yumin Jung \u001b[0m ☁️`)
+
+    print.newline()
+
+    print.highlight(`   UX Engineer \u001b[37mor \u001b[33mDevOps Engineer`)
+
+    print.newline()
+
+    print.info(
+      `   \u001b[47m\u001b[30m  GitHub \u001b[0m\u001b[4m https://github.com/yumin-jung\u001b[0m`
+    )
+    print.info(
+      `   \u001b[47m\u001b[30m    Blog \u001b[0m\u001b[4m https://yumin.blog\u001b[0m`
+    )
+
+    print.newline()
+
+    print.info(`   Good Luck 🍀`)
+
+    print.newline()
+    print.newline()
+    print.newline()
+  },
+}
+
+module.exports = command
