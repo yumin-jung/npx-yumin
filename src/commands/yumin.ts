e88df19f377e93fd9ebd8cb2a244cfddef1358a0
yumin-jung <jungym887@naver.com>
2024-10-11 17:07:09 +0900

    feat: Update npx-yumin v1.0.0

--- src/commands/yumin.ts
+++ src/commands/yumin.ts
@@ -9,27 +9,27 @@ const command: GluegunCommand = {
     print.newline()
     print.newline()
 
-    print.info(`   Hello, I'm \u001b[44m Yumin Jung \u001b[0m ☁️`)
+    print.info(`   Hello, I'm \u001b[30m\u001b[100m Yumin Jung \u001b[0m ☁️`)
 
     print.newline()
 
-    print.highlight(`   UX Engineer \u001b[37mand \u001B[93mMarathoner`)
+    print.highlight(`   Software Engineer`)
 
     print.newline()
 
     print.info(
-      `   \u001b[47m\u001b[30m  GitHub \u001b[0m\u001b[4m https://github.com/yumin-jung\u001b[0m`
+      `   \u001b[47m\u001b[30m   GitHub \u001b[0m\u001b[4m https://github.com/yumin-jung\u001b[0m`
     )
     print.info(
-      `   \u001b[47m\u001b[30m    Blog \u001b[0m\u001b[4m https://yumin.blog\u001b[0m`
+      `   \u001b[47m\u001b[30m     Blog \u001b[0m\u001b[4m https://yumin.blog\u001b[0m`
     )
     print.info(
-      `   \u001b[47m\u001b[30m  Strava \u001b[0m\u001b[4m https://www.strava.com/athletes/yumin-jung\u001b[0m`
+      `   \u001b[47m\u001b[30m LinkedIn \u001b[0m\u001b[4m https://www.linkedin.com/in/alex-yumin-jung/\u001b[0m`
     )
 
     print.newline()
 
-    print.info(`   🏃🏻‍➡️ Pain is inevitable, Suffering is optional.`)
+    print.info(`   ✨ No Silver Bullet ✨`)
 
     print.newline()
     print.newline()
