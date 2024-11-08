17746111f893615052dfc35c7f178541ed1a4cd2
yumin-jung <jungym887@naver.com>
2024-11-08 14:42:38 +0900

    feat: Update command line message

--- src/commands/yumin.ts
+++ src/commands/yumin.ts
@@ -9,7 +9,7 @@ const command: GluegunCommand = {
     print.newline()
     print.newline()
 
-    print.info(`   Hello, I'm \u001b[30m\u001b[100m Yumin Jung \u001b[0m ☁️`)
+    print.info(`   Hello, I'm Yumin Jung ☁️`)
 
     print.newline()
 
@@ -18,18 +18,13 @@ const command: GluegunCommand = {
     print.newline()
 
     print.info(
-      `   \u001b[47m\u001b[30m   GitHub \u001b[0m\u001b[4m https://github.com/yumin-jung\u001b[0m`
+      `   GitHub \u001b[0m\u001b[4m https://github.com/yumin-jung\u001b[0m`
     )
-    print.info(
-      `   \u001b[47m\u001b[30m     Blog \u001b[0m\u001b[4m https://yumin.blog\u001b[0m`
-    )
-    print.info(
-      `   \u001b[47m\u001b[30m LinkedIn \u001b[0m\u001b[4m https://www.linkedin.com/in/alex-yumin-jung/\u001b[0m`
-    )
+    print.info(`     Blog \u001b[0m\u001b[4m https://yumin.blog\u001b[0m`)
 
     print.newline()
 
-    print.info(`   ✨ No Silver Bullet ✨`)
+    print.info(`   \u001b[30m\u001b[100m WINNING ISN'T COMFORTABLE. `)
 
     print.newline()
     print.newline()
