9dc2b5939b372b1f2ccdc8bb6f80e3d171a67e89
yumin-jung <jungym887@naver.com>
2025-01-04 22:45:08 +0900

    feat: update links for GitHub and Blog

--- src/commands/yumin.ts
+++ src/commands/yumin.ts
@@ -137,11 +137,9 @@ const command: GluegunCommand = {
     print.info(`   \x1b[32mSoftware Engineer, Frontend\x1b[0m`)
     print.newline()
     print.info(
-      `   GitHub \u001b[0m\u001b[4m https://github.com/yumin-jung\u001b[0m`
+      `   [\u001b]8;;https://github.com/yumin-jung\u001b\\GitHub\u001b]8;;\u001b\\][\u001b]8;;https://yumin.me\u001b\\Blog\u001b]8;;\u001b\\]`
     )
-    print.info(`     Blog \u001b[0m\u001b[4m https://yumin.blog\u001b[0m`)
 
-    print.newline()
     print.newline()
 
     startLoading()
