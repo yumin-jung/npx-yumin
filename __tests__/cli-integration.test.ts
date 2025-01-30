f97538e65c86d04044e66c485b942cfabb6d2a58
yumin-jung <jungym887@naver.com>
2025-01-30 14:30:13 +0900

    feat: change links to specific stentences

--- __tests__/cli-integration.test.ts
+++ __tests__/cli-integration.test.ts
@@ -7,12 +7,12 @@ const cli = async (cmd) =>
 
 test('outputs version', async () => {
   const output = await cli('--version')
-  expect(output).toContain('1.4.0')
+  expect(output).toContain('1.5.0')
 })
 
 test('outputs help', async () => {
   const output = await cli('--help')
-  expect(output).toContain('1.4.0')
+  expect(output).toContain('1.5.0')
 })
 
 test('generates file', async () => {
--- src/commands/yumin.ts
+++ src/commands/yumin.ts
@@ -7,33 +7,46 @@ const command: GluegunCommand = {
 
     print.newline()
     print.newline()
+
+    print.info(`Hello, my name is Yumin Jung.`)
     print.newline()
 
-    print.info(`   Hello, I'm Yumin Jung`)
-    print.newline()
+    // print.info(`   \x1b[32mSoftware Engineer, Frontend\x1b[0m`)
+    // print.newline()
 
-    print.info(`   \x1b[32mSoftware Engineer, Frontend\x1b[0m`)
-    print.newline()
-
-    print.info(`   \x1b[104m   Work   \x1b[0m`)
-    print.newline()
-
-    print.info(`   [Blog]      \u001b[0m\u001b[4m https://yumin.me\u001b[0m`)
     print.info(
-      `   [LinkedIn]  \u001b[0m\u001b[4m https://www.linkedin.com/in/alex-yumin-jung\u001b[0m`
-    )
-    print.info(
-      `   [Baekjoon]  \u001b[0m\u001b[4m https://solved.ac/profile/self_overcoming\u001b[0m`
+      `I am a \x1b[32msoftware engineer\x1b[0m with a strong focus on frontend development, specializing in web, extended reality (XR), and iOS applications. With a background in UX/UI design, I strive to create seamless, intuitive, and user-centric digital experiences.`
     )
     print.newline()
 
-    print.info(`   \x1b[102m   Non-Work   \x1b[0m`)
+    print.info(
+      `I am driven by a passion for solving meaningful challenges, particularly in \x1b[32mdigital healthcare\x1b[0m and \x1b[32mmental well-being\x1b[0m. My goal is to develop technology that not only performs efficiently but also makes a tangible impact on people’s lives.`
+    )
+    print.newline()
+
+    print.info(`I would like to introduce some of my recent projects.`)
+    print.newline()
+
+    print.info(`- AutoPalp: AR-Powered Palpation Assistance`)
     print.newline()
 
     print.info(
-      `   [Strava]    \u001b[0m\u001b[4m https://www.strava.com/athletes/yumin-jung\u001b[0m`
+      `AutoPalp is an augmented reality (AR) software designed to empower individuals to perform palpation examinations with the accuracy of a trained physician. This project provided hands-on experience in integrating software solutions with medical applications, deepening my understanding of AR-based diagnostic tools. Additionally, I developed my expertise in Unity, focusing on its application in real-time spatial computing and user interaction.`
     )
     print.newline()
+
+    print.info(`- Depression Symptom Analysis & Data Collection App`)
+    print.newline()
+
+    print.info(
+      `I developed a research-driven iOS application for depression symptom analysis and data collection, with ResearchKit and CareKit to facilitate structured data gathering for clinical studies. This project allowed me to gain practical experience in iOS development for digital health research, focusing on patient-centric UX, secure data management, and real-world clinical integration.`
+    )
+    print.newline()
+    print.newline()
+
+    print.info(
+      `Beyond software engineering, I am a dedicated marathoner who thrives on endurance, discipline, and continuous improvement—principles that also shape my approach to engineering. I am also an persevering reader, constantly seeking new knowledge and perspectives to expand my thinking. Additionally, I have a deep appreciation for coffee, which fuels my creativity and focus throughout the day.`
+    )
     print.newline()
     print.newline()
   },
